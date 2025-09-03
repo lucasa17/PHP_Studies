87810bfa559d387bcf46f5d765e3035da86f648a
LUCAS SILVA DA ROSA <41279517883@CAMPUS.MACHADO.MCH>
2025-09-03 09:09:12 -0300

    feat: Exercicio com Get

---
+++ PHP-Basico/Activities/IF-ELSE/exercicio1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+    <?php
+        $year = isset($_GET['year']) ? $_GET['year'] : 'Ano não encontrado ou valor inválido';
+        
+        echo 'Ano digitado: ' . $year . '</br>  ';
+        echo 'Sua idade é ' . (date("Y") - $year) . '</br>  ';
+
+        echo (date("Y") - $year) >= 18 ? 'Pode votar' : 'Não pode votar';
+    ?>
+    </body>
+</html>
