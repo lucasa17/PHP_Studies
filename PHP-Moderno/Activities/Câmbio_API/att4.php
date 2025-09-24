a1afe439e4d57dcec2b7387a1e03573bf742e1a0
Lucasa17 <lucassilva04.rosa@gmail.com>
2025-09-24 15:48:40 -0300

    docs: Limpeza código att4

--- PHP-Moderno/Activities/Câmbio_API/att4.php
+++ PHP-Moderno/Activities/Câmbio_API/att4.php
@@ -19,14 +19,14 @@
             $dados = json_decode(file_get_contents($url), true);
             //var_dump($dados);
             $cotacao = $dados['value'][0]['cotacaoCompra'];
-            echo $cotacao;
-            
+
             $money = $_POST["intMoney"] ? floatval($_POST["intMoney"]):null ?? 0;
              
             echo "<p>Seus R$" . number_format($money, 2, ',') . " equivalem <strong>US$" .  number_format(($money/$cotacao), 2, ',') . "</strong></p>";
-            echo "Usando o câmbio de $cotacao - Segundo a API do Banco Centra do Brasil ";
+            echo "Usando o câmbio atual de $cotacao - Segundo a API do <a href='https://bcb.gov.br/'>Banco Centra do Brasil </a>";
             
         ?>
+        </br>
         <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
     </main>
 </body>
