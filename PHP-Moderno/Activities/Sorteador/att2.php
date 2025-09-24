d78296972bc56ded7827241103a64b6bfa09375a
Lucasa17 <lucassilva04.rosa@gmail.com>
2025-09-24 14:17:35 -0300

    feat: Atividade 2 - Sorteador

---
+++ PHP-Moderno/Activities/Sorteador/att2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Formulário</title>
+</head>
+<body>
+    <header>
+        <h1>Antecessor e Sucessor</h1>
+    </header>
+    <main>
+        <?php 
+            $minimo = $_POST["intMinimo"] ? intval($_POST["intMinimo"]):null ?? 0; 
+            $maximo = $_POST["intMaximo"] ? intval($_POST["intMaximo"]):null ?? 1; 
+
+            echo "<h2>Sorteando um valor aleatório entre $minimo e $maximo</h2>";
+            echo "Número sorteado: " . rand($minimo, $maximo);
+
+            echo "</br></br>";
+            echo "<button onclick='location.reload()'>Sortear novamente</button>";
+        ?>
+        <p><a href="index.html">Escolher outro intervalo</a></p>
+    </main>
+</body>
+</html>
